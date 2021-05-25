db02f88a10b3e8fa81143136f40c408caa1ba1e0
Matthew Casperson <matthewcasperson@gmail.com>
2021-05-25 12:46:38 +1000

    Cleaned imports

--- src/main/java/com/octopus/RandomQuotes.java
+++ src/main/java/com/octopus/RandomQuotes.java
@@ -10,7 +10,6 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.Random;
